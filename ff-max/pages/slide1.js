535770eb55e1696269baf79edc383e0fac3bdf25
log-del <95074741+log-del@users.noreply.github.com>
2021-12-17 14:04:29 +0200

    Update slide1.js

--- ff-max/pages/slide1.js
+++ ff-max/pages/slide1.js
@@ -1,73 +1,74 @@
+
 <div class="card-wrap">
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/1.jpeg">
+            <img src="https://i.ibb.co/GCFQNp9/1-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/2.jpeg">
+            <img src="https://i.ibb.co/cg7yMDD/2-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/3.jpeg">
+            <img src="https://i.ibb.co/vzyQMGn/3-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/4.jpeg">
+            <img src="https://i.ibb.co/xGVGSTJ/4-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/5.jpeg">
+            <img src="https://i.ibb.co/3hBcC56/5-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/6.jpeg">
+            <img src="https://i.ibb.co/K0zNxR1/6-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/7.jpeg">
+            <img src="https://i.ibb.co/xGbDVCB/7-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/8.jpeg">
+            <img src="https://i.ibb.co/0GYXrd0/8-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/9.jpeg">
+            <img src="https://i.ibb.co/fYD085k/9-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/10.jpeg">
+            <img src="https://i.ibb.co/0nyv91y/10-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/11.jpeg">
+            <img src="https://i.ibb.co/rQsQPg3/11-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
     <div class="card">
         <div class="imgBox">
-            <img src="img/senjata/12.jpeg">
+            <img src="https://i.ibb.co/QfpwXJg/12-min.png">
         </div>
         <div class="btn-claim" onclick="gass(this)">Claim</div>
     </div>
