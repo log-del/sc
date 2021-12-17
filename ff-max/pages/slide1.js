39bebeef8f43c77b8e1bd29258b9f4be4f8dbdf5
log-del <95074741+log-del@users.noreply.github.com>
2021-12-17 15:47:48 +0200

    Create slide1.js

---
+++ ff-max/pages/slide1.js
@@ -0,0 +1,74 @@
+<div class="card-wrap">
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/KmQ1CVF/1.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/CVYgCLP/2.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/6m4phz2/3.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/JcSk3Xk/4.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/C2Vt6JQ/5.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/HhkqT2G/6.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/p2bj9Sh/7.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/KLctszK/8.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/JyHf3Xt/9.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/hCNtbBK/10.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/pWxnRtj/11.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="https://i.ibb.co/P46kT41/12.jpg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+</div>
